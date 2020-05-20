ab37de1c7b28d9361cbc42b39c420527a6d1098e
Lyokoheros <44444444+Lyokoheros@users.noreply.github.com>
2020-05-20 18:32:51 +0200

    Lab9

---
+++ Lab 9 - ASP.NET MVC/MVCtutorial/MVCtutorial/Models/ViewModels/SongsListViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCtutorial.Models.ViewModels
+{
+    public class SongsListViewModel
+    {
+        public Song Song { get; set; }
+        public Genre Genre { get; set; }
+    }
+}
